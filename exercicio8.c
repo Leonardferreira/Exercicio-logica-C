b5ea5a9f0014b371b836ad56f61926a8802167b4
Leonardferreira <leonardosantospro@hotmail.com>
2024-11-12 15:00:52 -0300

    Conclusão

--- exercicio8.c
+++ exercicio8.c
@@ -1,4 +1,4 @@
-/*faça um programa que receba o valor de um depï¿½sito e o valor da taxa de juros, calcule e mostre o valor do rendimento e o valor
+/*faï¿½a um programa que receba o valor de um depï¿½sito e o valor da taxa de juros, calcule e mostre o valor do rendimento e o valor
 total depois do rendimento.*/
 
 
@@ -8,9 +8,19 @@ total depois do rendimento.*/
 
 int main(){
     setlocale(LC_ALL,"portuguese");
-    
+    float dep,rendi,valorTotal;
+    int tax;
 
+    printf("Insira o valora do deposito: \n");
+    scanf("%f", &dep);
+    printf("Insira a taxa a.m: \n");
+    scanf("%d",&tax);
 
+    rendi = dep + (dep * tax)/100;
+
+    valorTotal = dep + rendi;
+    printf("Rendimento: %.2f \n",rendi);
+    printf("Valor total: %.2f \n", valorTotal);
 
     return 0;
 }
